0f7d141f02873bd2110bc3dbf9b54926f62bc8ad
rhubbard1995 <rhubbard1995@hotmail.co.uk>
2013-02-13 19:44:31 +0000

    Update Lightswitch.js

--- Lightswitch.js
+++ Lightswitch.js
@@ -1,3 +1,10 @@
+/* Place this script on what ever you want to be your lightswitch in the hierarchy drag the light onto 
+   linkedlight. In the Hierarchy again click on the light you want to be turned on and off and tick it 
+   to off play the scene and left click it to work
+   
+   Script made by Robert Hubbard  Credits where ever if you wish
+*/
+
 var linkedLight : Light;
 
 function OnMouseDown() {
